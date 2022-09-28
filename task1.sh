aee4fa8b743177ab79f5885596957ae5b4ac27ef
FIXED-TERM Ly Thien Phu (MS/EMC23-XC) <lyp3hc@bosch.com>
2022-09-28 17:10:45 +0700

    Update task jenkins

--- task1.sh
+++ task1.sh
@@ -1,43 +1,47 @@
 #!/bin/bash
 ### Check script name hmi_base.sh exist or not, if not create it
-if [ ! -e /home/$USER/hmi_base.sh ]
+if [ ! -e ~/hmi_base.sh ]
   then
-    touch /home/$USER/hmi_base.sh
+    touch ~/hmi_base.sh
   else
-    echo "/home/$USER/hmi_base.sh is exist"
+    echo "~/hmi_base.sh is exist"
 fi
 ### Create folder name Demo
-if [ ! -d /home/$USER/Demo ]
+if [ ! -d ~/Demo ]
 then
-   mkdir /home/$USER/Demo
+   mkdir ~/Demo
 fi
 ### Create a text file with content "Hello world" in Demo folder, print info when finish
-if [ ! -e /home/$USER/Demo/hello.txt ]
+if [ ! -e ~/Demo/hello.txt ]
 then
-   touch /home/$USER/Demo/hello.txt
-   cd /home/$USER/Demo
+   touch ~/Demo/hello.txt
+   cd ~/Demo
    echo "Hello World" >> hello.txt
    cat hello.txt
 fi
-### Create folder_copy, file_copy.txt and file_copy_rsync.txt for task copy
-if [ ! -d /home/$USER/folder_copy ]
+### Create folder_copy for task copy
+
+if [ ! -d ~/folder_copy ]
 then
-   mkdir /home/$USER/folder_copy
+   mkdir ~/folder_copy
 fi
 ### Create file_copy.txt for task copy
-if [ ! -e /home/$USER/folder_copy/file_copy.txt ]
+if [ ! -e ~/folder_copy/file_copy.txt ]
 then
-   touch /home/$USER/folder_copy/file_copy.txt
+   touch ~/folder_copy/file_copy.txt
 fi
 ### Create file_copy_rsync.txt for task copy
-if [ ! -e /home/$USER/folder_copy/file_copy_rsync.txt ]
+if [ ! -e ~/folder_copy/file_copy_rsync.txt ]
 then
-   touch /home/$USER/folder_copy/file_copy_rsync.txt
+   touch ~/folder_copy/file_copy_rsync.txt
 fi
 ### Copy 1 file from other folder to Demo folder
+
 ### Using copy default
-cp /home/$USER/folder_copy/file_copy.txt /home/$USER/Demo
+cp ~/folder_copy/file_copy.txt ~/Demo
 ### Using rsync
-rsync -r /home/$USER/folder_copy/file_copy_rsync.txt /home/$USER/Demo
+rsync -r ~/folder_copy/file_copy_rsync.txt ~/Demo
+
 ### Print End when finish execute file
-echo "End
+echo "End"
+
